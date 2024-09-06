4e8a4ee9e2d7438b5be4018f9aa215f060cac5d6
AtroxTheFox <vykoukalstanislav1@gmail.com>
2024-09-06 14:32:37 +0200

    Add project files.

---
+++ Psswd-generator/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Psswd_generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            toolTip1 = new ToolTip(components);
+            toolTip2 = new ToolTip(components);
+            toolTip3 = new ToolTip(components);
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(27, 30);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(100, 24);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Lowercase";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(27, 60);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(101, 24);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Uppercase";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(27, 90);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(87, 24);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Numeric";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(27, 120);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(150, 24);
+            checkBox4.TabIndex = 3;
+            checkBox4.Text = "Special characters";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Location = new Point(29, 25);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(449, 183);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Options";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 240);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Password lenght:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(154, 238);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(59, 27);
+            numericUpDown1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 291);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(324, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 294);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Password:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(402, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 32);
+            button1.TabIndex = 9;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.AccessibleDescription = "";
+            button2.AccessibleName = "";
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(484, 291);
+            button2.Name = "button2";
+            button2.Size = new Size(35, 27);
+            button2.TabIndex = 10;
+            toolTip1.SetToolTip(button2, "Copy to Clipboard");
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // toolTip2
+            // 
+            toolTip2.ToolTipIcon = ToolTipIcon.Warning;
+            toolTip2.ToolTipTitle = "Warning";
+            // 
+            // toolTip3
+            // 
+            toolTip3.ToolTipIcon = ToolTipIcon.Info;
+            toolTip3.ToolTipTitle = "Copied to Clipboard";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(540, 380);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            ForeColor = SystemColors.ControlText;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Password Generator";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private GroupBox groupBox1;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private ToolTip toolTip1;
+        private ToolTip toolTip2;
+        private ToolTip toolTip3;
+    }
+}
---
+++ Psswd-generator/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Psswd_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            numericUpDown1.Value = 8; //výchozí délka hesla 8 znaků
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool lowercase = checkBox1.Checked; //booly pro zaškrtnutí checkboxu
+            bool uppercase = checkBox2.Checked;
+            bool numbers = checkBox3.Checked;
+            bool special = checkBox4.Checked;
+
+            int psswdLength = Convert.ToInt32(numericUpDown1.Value); //převod hodnoty na int z komponenty numericUpDown1
+
+            string password = GeneratePassword(lowercase, uppercase, numbers, special, psswdLength); //volání metody GeneratePassword; podle stavu boolů a int psswdLenght vygeneruje výsledné heslo po zmáčnknutí tlačítka "Generate"
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //tlačítko Copy to Clipboard
+        {
+            if (textBox1.Text != "") //pokud je v textboxu vygenerované heslo, po stisknutí tlačítka se zkopíruje do schránky
+            {
+                Clipboard.SetText(textBox1.Text);
+        
+                toolTip3.Show("Password copied to clipboard!", button2, 0, -40, 2000);
+
+            }
+            else
+            {
+                toolTip2.Show("No password to copy!", button2, 0, -40, 2000); //v opačném případě se uživateli zobrazí dialogové okno s chybou
+            }
+        }
+
+        private string GeneratePassword(bool lenght, bool uppercase, bool numbers, bool special, int psswdLength) //metoda pro generování hesla
+        {
+            string password = "";
+            string charSet = "";
+
+            if (lenght)
+            {
+                charSet += "abcdefghijklmnopqrstuvwxyz"; //pokud je zaškrtnutý checkbox "Lowercase", přidají se do charSetu malá písmena
+            }
+            if (uppercase)
+            {
+                charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//pokud je zaškrtnutý checkbox "Uppercase", přidají se do charSetu velká písmena
+            }
+            if (numbers)
+            {
+                charSet += "0123456789"; //pokud je zaškrtnutý checkbox "Numbers", přidají se do charSetu čísla
+            }
+            if (special)
+            {
+                charSet += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";//pokud je zaškrtnutý checkbox "Special", přidají se do charSetu speciální znaky
+            }
+
+            if (charSet == "")
+            {
+                MessageBox.Show("No character set selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return ""; //pokud není vybrán žádný znak, uživateli se zobrazí dialogové okno s chybou
+            }
+
+            Random rnd = new Random(); //vytvoření instance třídy Random
+            for (int i = 0; i < psswdLength; i++)//cyklus pro generování hesla
+            {
+                password += charSet[rnd.Next(0, charSet.Length)];//do proměnné password se přidávají náhodně vybrané znaky z charSetu
+            }
+
+            textBox1.Text = password;
+            return password;//vrácení vygenerovaného hesla
+        }
+    }
+}
